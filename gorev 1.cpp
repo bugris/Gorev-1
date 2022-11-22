68bcd114d0065dc614df3770786a42e3c0a5eb6f
bugris <118209677+bugris@users.noreply.github.com>
2022-11-22 11:18:01 +0300

    Add files via upload

---
+++ gorev 1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#define g 4.903325
+using namespace std;
+int main(){
+    
+      int saniye ;
+
+      cout << " lutfen bir saniye degeri giriniz:";
+      cin >> saniye;
+      cout << endl;
+
+    
+      cout << " cismin aldigi yol:" << g * saniye * saniye<< endl;
+    
+     
+}
+
+
+
